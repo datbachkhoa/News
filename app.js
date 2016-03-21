3fe4ef502b5b064b47c447962997c5338bbcff3b
Nguyen Ngoc Dat <datnn@qsoft.com.vn>
2016-03-21 22:11:42 +0700

    add login folder

--- app.js
+++ app.js
@@ -8,15 +8,15 @@ Ext.application({
 
     extend: 'News.Application',
 
-    requires: [
-        'News.view.main.Main'
-    ],
+    // requires: [
+    //     'News.view.main.Main'
+    // ],
 
     // The name of the initial view to create. With the classic toolkit this class
     // will gain a "viewport" plugin if it does not extend Ext.Viewport. With the
     // modern toolkit, the main view will be added to the Viewport.
     //
-    mainView: 'News.view.main.Main'
+    // mainView: 'News.view.main.Main'
 	
     //-------------------------------------------------------------------------
     // Most customizations should be made to News.Application. If you need to
